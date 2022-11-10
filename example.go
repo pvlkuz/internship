26efa39e9bbe9ec5dcdd6b8e2b5a5f84c88d774f
pvlkuz <pvlkuz@gmail.com>
2022-11-10 10:03:31 +0200

    made first task (1 argument received and returned to a console reversed, Go executable builded)

--- example.go
+++ example.go
@@ -7,7 +7,17 @@ import (
 	"os"
 )
 
+func reverse(s *string) string {
+	rns := []rune(*s)
+	for i, j := 0, len(rns)-1; i < j; i, j = i+1, j-1 {
+		rns[i], rns[j] = rns[j], rns[i]
+	}
+	return string(rns)
+}
+
 func main() {
-	fmt.Fprintln(os.Stdout, "hello, world")
-	fmt.Println("HELLO, WORLD")
+	//fmt.Fprintln(os.Stdout, "hello, world")
+
+	arg := os.Args[1]
+	fmt.Println(reverse(&arg))
 }
